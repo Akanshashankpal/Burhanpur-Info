fda7febfbca91f6c68c49b651de27c623ca3c330
Akanshashankpal <aakankshashankpal38@gmail.com>
2025-05-19 11:43:32 +0530

    Local Changes

--- OurCity/axios.js
+++ OurCity/axios.js
@@ -1,14 +1,24 @@
 // src/axios.js
 
-import axios from "axios";
+import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://burhanpur-city-backend.vercel.app/api/',  
+  baseURL: 'https://burhanpur-city-backend.vercel.app/api/',
   headers: {
     'Content-Type': 'application/json',
-    // Authorization: `Bearer ${token}`, 
   },
-  timeout: 10000, 
+  timeout: 10000,
+});
+
+// Attach token from localStorage to every request
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default axiosInstance;
--- OurCity/src/Components/Page/CatagoriesPart/ITPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ITPage = () => {
-  return (
-    <div>ITPage</div>
-  )
-}
-
-export default ITPage
